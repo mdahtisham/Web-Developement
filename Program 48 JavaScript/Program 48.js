692c65cd3efd1eae5eabd9ec763ab8c235826433
Md Ahtisham <mohdahtisham9889@gmail.com>
2022-02-26 06:06:02 -0800

    Web Developement

---
+++ Program 48 JavaScript/Program 48.js
@@ -0,0 +1,20 @@
+console.log("This is a Program");
+
+let names = "Coder";
+let name1 = "Coder1";
+let name2 = "Coder2";
+let name3 = "Coder3";
+console.log(names + " is a good programmer")
+console.log(name1 + " is a good programmer")
+console.log(name2 + " is a good programmer")
+console.log(name3 + " is a good programmer")
+
+
+function greet(names){
+    console.log(names + " is a good boy");
+}
+
+greet(names);
+greet(name1);
+greet(name2);
+greet(name3);
